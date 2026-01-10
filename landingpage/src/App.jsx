71e0f52ce44aa8359f6ad6fcb36c02b7ca35e10a
CWFlemmer <76725624+CWFlemmer@users.noreply.github.com>
2026-01-10 11:19:10 +0200

    .

--- landingpage/src/App.jsx
+++ landingpage/src/App.jsx
@@ -46,7 +46,7 @@ const App = () => {
           navigateToDemo={navigateToDemo}
           scrollToSection={scrollToSection}
         />
-        <LiveSensorStats />
+        {/* <LiveSensorStats /> */}
         <ColdChain />
         <AboutUs />
         <ContactUs />
